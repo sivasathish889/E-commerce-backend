9ab853430f1880c3cbddbb7d81cf9c86bb17b356
sivasathish889 <sivasathish889@gmail.com>
2026-02-20 19:27:50 +0530

    update controller

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
+  credentials: true,  
 
 }));
 app.use(bodyParser.json());
--- src/controller/category.controller.ts
+++ src/controller/category.controller.ts
@@ -2,44 +2,40 @@ import type { Request, Response } from "express";
 import { prisma } from "../prisma/index.js";
 import asyncHandler from "express-async-handler";
 
-export const addCategory = async (req: Request, res: Response) => {
-  try {
-    const { name, description } = req.body;
-    const image = req.file?.path;
-    if (!name || !image) {
-      res
-        .status(400)
-        .json({ message: "Please fill all the fields", success: false });
-      return;
-    }
-    const isCategoryExist = await prisma.category.findFirst({
-      where: {
-        name: name,
-      },
-    });
-    if (isCategoryExist) {
-      res
-        .status(400)
-        .json({ message: "Category already exists", success: false });
-      return;
-    }
-    const category = await prisma.category.create({
-      data: {
-        name,
-        picture: image,
-        description: description,
-      },
-    });
-    res.status(200).json({
-      message: "Category added successfully",
-      success: true,
-      category,
-    });
+export const addCategory = asyncHandler(async (req: Request, res: Response) => {
+  const { name, description } = req.body;
+  const image = req.file?.path;
+  if (!name || !image) {
+    res
+      .status(400)
+      .json({ message: "Please fill all the fields", success: false });
     return;
-  } catch (error) {
-    return res.status(500).json({ message: error, success: false });
   }
-};
+  const isCategoryExist = await prisma.category.findFirst({
+    where: {
+      name: name,
+    },
+  });
+  if (isCategoryExist) {
+    res
+      .status(400)
+      .json({ message: "Category already exists", success: false });
+    return;
+  }
+  const category = await prisma.category.create({
+    data: {
+      name,
+      picture: image,
+      ...(description && { description }),
+    },
+  });
+  res.status(200).json({
+    message: "Category added successfully",
+    success: true,
+    category,
+  });
+  return;
+});
 
 export const getCategory = asyncHandler(async (req: Request, res: Response) => {
   const categories = await prisma.category.findMany();
@@ -71,7 +67,7 @@ export const getCategoryById = asyncHandler(
 export const updateCategory = asyncHandler(
   async (req: Request, res: Response) => {
     const { id } = req.params;
-    const { name } = req.body;
+    const { name, description } = req.body;
     const image = req.file?.path;
     const category = await prisma.category.update({
       where: {
@@ -80,6 +76,7 @@ export const updateCategory = asyncHandler(
       data: {
         ...(name && { name: name }),
         ...(image && { picture: image }),
+        ...(description && { description }),
       },
     });
     res.status(200).json({
